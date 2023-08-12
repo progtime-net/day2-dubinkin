2ef82ac3f0279c8105d0d520755ba9b8082ecaa6
AnloGames <7383an@gmail.com>
2023-08-12 12:28:19 +0300

    greetingsName

--- GitHubProj_02/Program.cs
+++ GitHubProj_02/Program.cs
@@ -1,5 +1,3 @@
 ﻿Console.WriteLine("Здравствуйте, введите ваше имя");
 string name = Console.ReadLine();
-Console.WriteLine("Введите возраст");
-int age =int.Parse(Console.ReadLine());
-Console.WriteLine("Привет, " + name + " " + age);
+Console.WriteLine("Привет, " + name);
